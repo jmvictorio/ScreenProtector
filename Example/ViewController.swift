7896d0f2e9fea117d33765a9871e284a9d750567
jvictorio <jvictorio@deloitte.es>
2020-02-04 12:44:59 +0100

    not finish pod

---
+++ Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by jvictorio on 04/02/2020.
+//  Copyright © 2020 jvictorio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,7 @@
+
+
+target 'Example' do
+  platform :ios, '9.0'
+  use_frameworks!
+  pod 'ScreenProtector'
+end
